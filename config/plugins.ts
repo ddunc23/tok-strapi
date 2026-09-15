523237fe8098c169c79394bc36845a41b3384da3
ddunc23 <d.hay@ucl.ac.uk>
2026-09-15 10:21:34 +0100

    added caching for expensive routes

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,27 @@
-export default () => ({});
+export default () => ({
+    'rest-cache': {
+    config: {
+      provider: {
+        name: 'memory',
+        options: {
+          maxSize: 32767,
+        },
+      },
+      strategy: {
+        contentTypes: [
+          'api::term.term',
+          'api::maker-term-association.maker-term-association',
+          'api::town-location.town-location',
+          'api::vocabulary.vocabulary',
+          'api::guild.guild',
+          {
+            contentType: 'api::maker-extended.maker-extended',
+            routes: [
+                '/api/maker-extendeds/facet-counts',
+            ]
+          }
+        ],
+      },
+    },
+  },
+});
